8eade60a6780042356493c2e2c2eade68df3547f
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-03 13:33:14 +0200

    EXO1

---
+++ assets/js/Exo1.js
@@ -0,0 +1,10 @@
+function multiply(form1){
+    let num1 = document.form1.input1.value;
+    let num2 = document.form1.input2.value;
+    let result = 0;
+
+    result = num1 * num2;
+
+    document.form1.output.value = result;
+}
+
